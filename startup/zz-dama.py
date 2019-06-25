03eb955dbe4be9e09d7c7be152804e5da295ca6e
Maksim Rakitin <mrakitin@bnl.gov>
2019-06-25 14:33:31 -0400

    Prototype an EpicsMotor with limits

---
+++ startup/zz-dama.py
@@ -0,0 +1,8 @@
+from ophyd import EpicsMotor                                                                       
+
+
+class EpicsMotorWithLimits(EpicsMotor): 
+    low_limit = Cpt(EpicsSignal, '.LLM')
+    high_limit = Cpt(EpicsSignal, '.HLM')
+
+ir_with_limits = EpicsMotorWithLimits('XF:12ID1-ES{XtalDfl-Ax:IR}Mtr', name='ir_with_limits')
