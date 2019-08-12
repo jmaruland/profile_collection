cacbe2ad4efdf4032cbe8e682b4ca47574bcff79
JPLS Operator <xf12id1@xf12id1-ws1.cs.nsls2.local>
2019-08-12 12:20:57 -0400

    Implementation of a moving function from a Given PseudoPosition as well as an automatic tracking of ih and phi motors

--- startup/30-geometry.py
+++ startup/30-geometry.py
@@ -10,8 +10,8 @@ class Geometry(PseudoPositioner):
     alpha = Cpt(PseudoSingle, '', labels=['geo'])
 
     th = Cpt(EpicsMotor, '{XtalDfl-Ax:Th}Mtr', labels=['geo'])
-#    phi = Cpt(EpicsMotor, '{XtalDfl-Ax:Phi}Mtr', labels=['geo'])   
-    phi = Cpt(EpicsMotor, '{XtalDfl-Ax:M7}Mtr', labels=['geo'])   
+    phi = Cpt(EpicsMotor, '{XtalDfl-Ax:Phi}Mtr', labels=['geo'])   
+#    phi = Cpt(EpicsMotor, '{XtalDfl-Ax:M7}Mtr', labels=['geo'])   
     chi = Cpt(EpicsMotor, '{XtalDfl-Ax:Chi}Mtr', labels=['geo'])
     tth = Cpt(EpicsMotor, '{XtalDfl-Ax:Tth}Mtr', labels=['geo'])
     ih = Cpt(EpicsMotor, '{XtalDfl-Ax:IH}Mtr', labels=['geo'])
@@ -19,11 +19,11 @@ class Geometry(PseudoPositioner):
 
     def __init__(self, prefix, **kwargs):
         self.wlength = 0.77086  # x-ray wavelength, A       
-        self.s_l1 = 300 # distance crystal to input arm elevator, mm
+        self.s_l1 = 278 # distance crystal to input arm elevator, mm
         self.s_l2 = 850 # distance crystal to sample center, mm 
         self.s_13 = 600 # distance sample to output elevator, mm
         self.s_qtau = 1.9236 # monochromator reciprocal lattice vector, 1/A 
-        self.s_Eta = 0 # inc beam upward tilt from mirror (rad) 
+        self.s_Eta = 0.000722 # inc beam upward tilt from mirror (rad) 
         self.s_trck = 0 # whether to track sample table
         super().__init__(prefix, **kwargs)   
         # For some weird reason we need to give it some time to connect in the simulation mode
@@ -35,7 +35,7 @@ class Geometry(PseudoPositioner):
                                    f'Make sure to connect it to use the "{self.name}" pseudopositioner.')
 
     @pseudo_position_argument
-    def forward(self, pseudo_pos):
+    def ca(self, pseudo_pos):
         '''Calculate a RealPosition from a given PseudoPosition
            based on the s_motors method
 
@@ -117,6 +117,14 @@ class Geometry(PseudoPositioner):
         return self.RealPosition(th=_th, phi=_phi, chi=_chi,
                                  tth=_tth, ih=_ih, ir=_ir)
 
+    def move_ca(self, pseudo_pos):
+        temp = self.ca(pseudo_pos)
+        yield from bps.mv(geo.tth, temp.tth)
+        yield from bps.mv(geo.phi, temp.phi)
+        yield from bps.mv(geo.chi, temp.chi)
+        yield from bps.mv(geo.ir, -temp.ir)
+        yield from bps.mv(geo.ih, temp.ih)
+
     @real_position_argument
     def inverse(self, real_pos):
         '''Calculate a PseudoPosition from a given RealPosition
@@ -152,3 +160,56 @@ class Geometry(PseudoPositioner):
 
 #geo = Geometry('SXF:12ID1-ES', name='geo')
 geo = Geometry('XF:12ID1-ES', name='geo')
+
+
+def phi_track(alpha_ini, alpha_stop, num_alpha):
+    #for eta in range(eta_start, eta_stop, nb_eta):
+    #eta
+    
+    dif = np.zeros((2, num_alpha))
+    for i, alpha in enumerate(range(0, num_alpha, 1)):
+        alpha_re = alpha_ini + (i*(alpha_stop - alpha_ini)/num_alpha)
+        print(i, alpha_ini, alpha_re)
+        yield from geo.move_ca(alpha_re)
+        yield from bp.rel_scan([quadem],geo.phi,-0.02,0.01,60)
+        print(peaks.cen['quadem_current2_mean_value'] - geo.ca(alpha_re).phi)  
+        dif[0, i] = alpha_re
+        dif[1, i] = peaks.cen['quadem_current2_mean_value'] - geo.ca(alpha_re).phi
+
+    print(dif)
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.plot(dif[0,:],dif[1, :])
+    plt.show()
+
+
+
+def ih_track(alpha_ini, alpha_stop, num_alpha):
+    #for ih in range(alpha_ini,alpha_stop, nb_alpha):
+    
+    dif = np.zeros((3, num_alpha))
+    for i, alpha in enumerate(range(0, num_alpha, 1)):
+        alpha_re = alpha_ini + (i*(alpha_stop - alpha_ini)/num_alpha)
+        print(i, alpha_ini, alpha_re)
+        yield from geo.move_ca(alpha_re)
+        yield from bp.rel_scan([quadem],geo.phi,0.010,-0.010,20)
+        yield from bps.mv(geo.phi,peaks.cen['quadem_current2_mean_value'])
+        dif[2, i] = peaks.cen['quadem_current2_mean_value'] - geo.ca(alpha_re).phi
+        yield from bp.rel_scan([quadem],geo.ih,-0.5,0.5,20)
+# is the next line corrct, geo.ca. 
+        print(peaks.cen['quadem_current3_mean_value'] - geo.ca(alpha_re).ih)  
+        dif[0, i] = alpha_re
+        dif[1, i] = peaks.cen['quadem_current3_mean_value'] - geo.ca(alpha_re).ih 
+
+    print(dif)
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.subplot(211)
+    plt.title('IH_track')
+    plt.plot(dif[0,:],dif[1, :])
+    plt.subplot(212)
+    plt.title('Phi_track')
+    plt.plot(dif[0,:],dif[2, :])
+    plt.show()
+
+
