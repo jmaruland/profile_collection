02732e649d1f361021775864b6a06467774d0983
JPLS Operator <xf12id1@xf12id1-ws1.cs.nsls2.local>
2020-11-19 09:32:38 -0500

    Add vfm and hfm slits

--- startup/25-slits.py
+++ startup/25-slits.py
@@ -24,3 +24,16 @@ class Smaract1(Device):
 S1 = SlitsWithTopBottomInbOutb("XF:12ID1-ES{Slt1-Ax:", name="S1")
 S2 = SlitsWithGapAndCenter("XF:12ID1-ES{Slt2-Ax:", name="S2")
 S3 = Smaract1("XF:12ID1-ES{SM:1-Ax:", name="S3")
+
+
+class SLTH(Device):
+    h = Cpt(EpicsMotor, 'Hpos}Mtr')
+    hg = Cpt(EpicsMotor, 'Hgap}Mtr')
+
+class SLTV(Device):
+    v = Cpt(EpicsMotor, 'Vpos}Mtr')
+    vg = Cpt(EpicsMotor, 'Vgap}Mtr')
+
+# FOE mono beam slits
+hfmslit = SLTH('XF:12IDA-OP:2{Slt:H-Ax:', name='hfmslit')
+vfmslit = SLTV('XF:12IDA-OP:2{Slt:V-Ax:', name='vfmslit')
