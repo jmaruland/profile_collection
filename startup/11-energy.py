501a260fe71236e4e62e98372371a402df47b433
Guillaume Freychet <gfreychet@gmail.com>
2020-11-21 16:28:06 -0500

    Adjust the gap value for 9.7 keV

--- startup/11-energy.py
+++ startup/11-energy.py
@@ -29,7 +29,7 @@ def energy_to_gap(target_energy, undulator_harmonic=1):
     b7 = -1.70455e-22
     gap_mm = a + b1*f + b2*f**2 + b3 * f**3 + b4 * f**4 + b5 * f**5 + b6 * f**6 + b7 * f**7
     # -33 for 14 keV; -21 for 16.1 keV; -50 for 9540eV; -20 for 2450eV; -45 for 4050eV
-    gap = gap_mm*1000 - 20
+    gap = gap_mm*1000 - 35
     return gap
 
 
