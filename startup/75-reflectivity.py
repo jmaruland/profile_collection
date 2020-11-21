68cf523bf6247a30d3b6d624c39bc2cea873df4b
Guillaume Freychet <gfreychet@gmail.com>
2020-11-21 16:27:38 -0500

    Clean the typos added between teh 2 cycles. Move teh GID code in a separate file

--- startup/75-reflectivity.py
+++ startup/75-reflectivity.py
@@ -8,8 +8,7 @@ all_area_dets = [lambda_det, quadem]
 
 
 @bpp.stage_decorator(all_area_dets)
-def reflection_scan(alpha_start, alpha_stop, num, detector='lambda_det', precount_time=1, exp_time=1,
-                    default_att=1e-7, md=None):
+def reflection_scan(alpha_start, alpha_stop, num, detector='lambda_det', precount_time=1, exp_time=1, default_att=1e-7, md=None):
 
     for alpha in np.linspace(alpha_start, alpha_stop, num):
         # Move to the good geometry position
@@ -60,14 +59,14 @@ def reflection_scan(alpha_start, alpha_stop, num, detector='lambda_det', precoun
                 i_max = ret['%s_stats4_max_value'%detector]['value']
                         
             # Adjust the absorbers to avoid saturation of detector
-            best_att, attenuation_factor, best_att_name = calculate_and_set_absorbers(energy=energy.energy.position,
-                                                                                      i_max=i_max,
-                                                                                      att=def_att,
-                                                                                      precount_time=precount_time)
+            best_att, attenuation_factor, best_att_name = yield from calculate_and_set_absorbers(energy=energy.energy.position,
+                                                                                                i_max=i_max,
+                                                                                                att=def_att,
+                                                                                                precount_time=precount_time)
             
             # Upload the attenuation factor for the metadata
             yield from bps.mv(attenuation_factor_signal, attenuation_factor)
-            yield from bps.mv(atenuator_name_signal, best_att_name)
+            yield from bps.mv(attenuator_name_signal, best_att_name)
 
         # Set the exposure time to the define exp_time for the measurement
         yield from det_exposure_time(exp_time, exp_time)
@@ -94,7 +93,7 @@ def night_scan():
 
 
 def fast_scan():
-    yield from expert_reflection_scan(md={'sample_name': 'water_reigler_fast1'})
+    yield from expert_reflection_scan(md={'sample_name': 'water_large'})
 
 
 def expert_reflection_scan(md=None, detector='lambda_det'):
@@ -114,7 +113,7 @@ def expert_reflection_scan(md=None, detector='lambda_det'):
     base_md = {'plan_name': 'reflection_scan',
                'detector': detector, 
                'energy': energy.energy.position,
-               'rois': [200, 278, 298, 318]
+               'rois': [203, 193, 207, 221]
                }
     base_md.update(md or {})
 
@@ -134,7 +133,7 @@ def expert_reflection_scan(md=None, detector='lambda_det'):
     print('1st set starting')
 
     # Move stable X2
-    yield from bps.mvr(geo.stblx2, -0.5)
+    #yield from bps.mvr(geo.stblx2, -0.5)
     yield from bps.sleep(5)
     alpha_start, alpha_stop, num, exp_time, precount_time = 0.05, 0.2, 20, 2, 0.1
     yield from reflection_scan(alpha_start=alpha_start,
@@ -143,66 +142,71 @@ def expert_reflection_scan(md=None, detector='lambda_det'):
                                detector=detector,
                                precount_time=precount_time,
                                exp_time=exp_time,
+                               default_att = default_attenuation.value,
                                md=md)
     
     print('1st set done')
     print('2nd set starting')
 
     # Move stable X2
-    yield from bps.mvr(geo.stblx2, -0.5)
+   # yield from bps.mvr(geo.stblx2, -0.5)
     yield from bps.sleep(5)
     alpha_start, alpha_stop, num, exp_time, precount_time = 0.2, 0.6, 17, 2, 0.1
     yield from reflection_scan(alpha_start=alpha_start,
                                alpha_stop=alpha_stop,
                                num=num,
                                detector=detector,
-                               exp_time=exp_time,
                                precount_time=precount_time,
+                               exp_time=exp_time,
+                               default_att = default_attenuation.value,
                                md=md)
     
     print('2nd set done', 'default attenuation is', default_attenuation)
     print('3rd set starting')
 
     # Move stable X2
-    yield from bps.mvr(geo.stblx2, -0.5)
+    #yield from bps.mvr(geo.stblx2, -0.5)
     yield from bps.sleep(5)
     alpha_start, alpha_stop, num, exp_time, precount_time = 0.6, 1, 9, 5, 0.1
     yield from reflection_scan(alpha_start=alpha_start,
                                alpha_stop=alpha_stop,
                                num=num,
                                detector=detector,
-                               exp_time=exp_time,
                                precount_time=precount_time,
+                               exp_time=exp_time,
+                               default_att = default_attenuation.value,
                                md=md)
 
     print('3rd set done')
     print('4th set starting')
 
     # Move stable X2
-    yield from bps.mvr(geo.stblx2, -0.5)
+    #yield from bps.mvr(geo.stblx2, -0.5)
     yield from bps.sleep(5)
     alpha_start, alpha_stop, num, exp_time, precount_time = 1, 2, 11, 5, 0.1 
     yield from reflection_scan(alpha_start=alpha_start,
                                alpha_stop=alpha_stop,
                                num=num,
                                detector=detector,
-                               exp_time=exp_time,
                                precount_time=precount_time,
+                               exp_time=exp_time,
+                               default_att = default_attenuation.value,
                                md=md)
 
     print('4th set done')
     print('5th set starting')
 
     # Move stable X2
-    yield from bps.mvr(geo.stblx2, -0.5)
+    #yield from bps.mvr(geo.stblx2, -0.5)
     yield from bps.sleep(5)
     alpha_start, alpha_stop, num, exp_time, precount_time = 2, 3, 11, 10, 0.1
     yield from reflection_scan(alpha_start=alpha_start,
                                alpha_stop=alpha_stop,
                                num=num,
                                detector=detector,
-                               exp_time=exp_time,
                                precount_time=precount_time,
+                               exp_time=exp_time,
+                               default_att = default_attenuation.value,
                                md=md)
 
     print('5th set done')
@@ -215,96 +219,3 @@ def expert_reflection_scan(md=None, detector='lambda_det'):
 
     yield from bps.mv(abs2, 5)
     print('The reflectivity scan is over')
-
-
-gid_dets = [pilatus100k]
-@bpp.stage_decorator(gid_dets)
-def gid(md=None, energy=9700, exp_time=1, detector = 'pilatus100k'):
-    # Bluesky command to record metadata
-    base_md = {'plan_name': 'gid',
-               'detector': detector, 
-               'energy': energy.energy.position,
-            # ...
-           }
-    base_md.update(md or {})
-    bec.disable_plots()
-    yield from bps.open_run(md=base_md)
-
-    
-    # Creation of a fignal to record the attenuation
-    attenuation_factor_signal = Signal(name='attenuation', value = 1)
-
-    exposure_time = Signal(name='exposure_time', value = 1)
-
-    # Move the default attenuator in for the precount
-    def_att = yield from put_default_absorbers(energy)
-
-    # Set the exposure time to 0.1 for the precount
-    det_exposure_time(0.1, 0.1)
-
-    # Move to the good geometry position
-    yield from bps.mv(shutter,0)
-    # yield from mabt(0.1,1,0.6) # gid poistion with beam stop
-    yield from mabt(0.1,1.5,0.38) # gid poistion with beam stop
-
-    yield from bps.sleep(5)
-    yield from bps.mv(shutter,1)
-
-    # Take the pre-count data
-    ret = yield from bps.trigger_and_read(area_dets, name='precount')
-    yield from bps.mv(shutter,0)
-
-    # yield from bps.mv(abs2, 0)
-    yield from bps.mv(abs2, 3)# to avoid pilatus saturation
-    yield from bps.mv(attenuation_factor_signal, 1)
-
-    # Set the exposure time to the define exp_time for the measurment
-    det_exposure_time(exp_time, exp_time)
-    yield from bps.mv(exposure_time, exp_time)
-    yield from bps.mvr(geo.stblx2, -1) # move stable X2
-    print(exposure_time.get())
-    yield from bps.mv(shutter,1)
-    yield from bps.sleep(0.5) # add this because the QuadEM I0
-    yield from bps.trigger_and_read(area_dets + [geo] + [attenuation_factor_signal] + [exposure_time], name='primary')
-    yield from bps.mv(shutter,0)
-
-    yield from bps.mv(abs2, 5)
-    yield from mabt(0.1,1.5,0) # gid poistion without beam stop
-    yield from bps.sleep(5)
-    yield from bps.mv(exposure_time, 1)
-    yield from bps.mv(shutter,1)
-    yield from bps.sleep(0.5) # add this because the QuadEM I0
-    yield from bps.trigger_and_read(area_dets + [geo] + [attenuation_factor_signal] + [exposure_time], name='primary')
-    yield from bps.mv(shutter,0)
-
-    yield from mabt(0.1,1.5,0.76) # gid poistion without beam stop
-    yield from bps.sleep(5)
-    yield from bps.mv(exposure_time, 1)
-    yield from bps.mv(shutter,1)
-    yield from bps.sleep(0.5) # add this because the QuadEM I0
-    yield from bps.trigger_and_read(area_dets + [geo] + [attenuation_factor_signal] + [exposure_time], name='primary')
-    yield from bps.mv(shutter,0)
-
-    yield from mabt(0.1,1.5,-0.38) # gid poistion without beam stop
-    yield from bps.sleep(5)
-    yield from bps.mv(exposure_time, 1)
-    yield from bps.mv(shutter,1)
-    yield from bps.sleep(0.5) # add this because the QuadEM I0
-    yield from bps.trigger_and_read(area_dets + [geo] + [attenuation_factor_signal] + [exposure_time], name='primary')
-    yield from bps.mv(shutter,0)
-
-    yield from mabt(0.1,1.5,-0.76) # gid poistion without beam stop
-    yield from bps.sleep(5)
-    yield from bps.mv(exposure_time, 1)
-    yield from bps.mv(shutter,1)
-    yield from bps.sleep(0.5) # add this because the QuadEM I0
-    yield from bps.trigger_and_read(area_dets + [geo] + [attenuation_factor_signal] + [exposure_time], name='primary')
-    yield from bps.mv(shutter,0)
-
-
-
-    # Bluesky command to stop recording metadata
-    yield from close_run()
-    bec.enable_plots()
-    yield from bps.mv(abs2, 5)
-    print('The gid is over')
