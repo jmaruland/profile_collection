cededa47f2285da7776255aa8e0243f60e7be7ae
Guillaume Freychet <gfreychet@gmail.com>
2021-03-09 12:02:42 -0500

    Adding a code to automatically create directory for each new user

---
+++ startup/80-congig.py
@@ -0,0 +1,35 @@
+def proposal_id(cycle_id, proposal_id):
+    RE.md['cycle'] = cycle_id
+    RE.md['proposal_number'] = proposal_id.split('_')[0]
+    RE.md['main_proposer'] = proposal_id.split('_')[1]
+
+    # 2018-04-10: Maksim asked Tom about why this 'put' does not create the folder,
+    # Tom suggested to ask PoC to update AD installation.
+    import stat
+    newDir = "/nsls2/xf12id1/users/" + str(cycle_id) + "/" + str(proposal_id) + "/GID_data"
+    try:
+        os.stat(newDir)
+    except FileNotFoundError:
+        os.makedirs(newDir)
+        os.chmod(newDir, stat.S_IRWXU + stat.S_IRWXG + stat.S_IRWXO)
+    
+    newDir = "/nsls2/xf12id1/users/" + str(cycle_id) + "/" + str(proposal_id) + "/GID_analysis"
+    try:
+        os.stat(newDir)
+    except FileNotFoundError:
+        os.makedirs(newDir)
+        os.chmod(newDir, stat.S_IRWXU + stat.S_IRWXG + stat.S_IRWXO)
+
+    newDir = "/nsls2/xf12id1/users/" + str(cycle_id) + "/" + str(proposal_id) + "/XRR_analysis"
+    try:
+        os.stat(newDir)
+    except FileNotFoundError:
+        os.makedirs(newDir)
+        os.chmod(newDir, stat.S_IRWXU + stat.S_IRWXG + stat.S_IRWXO)
+        
+    newDir = "/nsls2/xf12id1/users/" + str(cycle_id) + "/" + str(proposal_id) + "/XRR_data"
+    try:
+        os.stat(newDir)
+    except FileNotFoundError:
+        os.makedirs(newDir)
+        os.chmod(newDir, stat.S_IRWXU + stat.S_IRWXG + stat.S_IRWXO)
