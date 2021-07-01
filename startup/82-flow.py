0544fcfbe30be1a7f0564ef46c3adb3cb5bb9763
Guillaume Freychet <gfreychet@gmail.com>
2021-07-01 11:40:17 -0400

    changes for feedback options

--- startup/82-flow.py
+++ startup/82-flow.py
@@ -1,1 +1,14 @@
 flow3 = EpicsSignal("XF:12ID1-ECAT:EL4104-00-AO3", name="flow3")
+he_pid = EpicsSignal("XF:12ID1-ES{He-Flow}PID1.FBON", name = "feed back on/off")
+
+def he_on():
+    yield from mov(he_pid,1)
+    yield from mov(flow3,2.7)
+
+def he_off():
+    yield from mov(he_pid,0)
+    yield from mov(flow3,0)
+
+
+
+
