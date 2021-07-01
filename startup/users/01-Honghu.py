0c1615d84b518f409b8268f68bb7f0403c0e902e
Guillaume Freychet <gfreychet@gmail.com>
2021-07-01 11:38:23 -0400

    Change user files after running 2021_2

--- startup/users/01-Honghu.py
+++ startup/users/01-Honghu.py
@@ -58,9 +58,9 @@ def cfn(name):
 
 
 
-gid_dets = [pilatus100k, quadem]
+gid_dets = [pilatus300k, quadem]
 @bpp.stage_decorator(gid_dets)
-def gid_cfn_cal(md=None, exp_time=1, detector = 'pilatus100k', alphai = 0.1, attenuator=2):
+def gid_cfn_cal(md=None, exp_time=1, detector = 'pilatus300k', alphai = 0.1, attenuator=2):
     # Bluesky command to record metadata
     base_md = {'plan_name': 'gid',
                'detector': detector, 
@@ -122,13 +122,93 @@ def gid_cfn_cal(md=None, exp_time=1, detector = 'pilatus100k', alphai = 0.1, att
     # yield from bps.mv(abs2, 5)
     print('The gid is over')
                        
-                       
+
+
+def cfn_3():
+
+    # name_cfn = { 1: 'AuNR_5_19_stock',
+    #              2: 'AuNR_5_19_T6K',
+    #              3: 'AuNR_5_19_E6K',
+    # }
+
+    # name_cfn = { 1: 'AuNR_5_19_stock_10mMNaCl', # add 20.2uL 1M NaCl @9:35pm 06/30/21
+    #              2: 'AuNR_5_19_T6K_10mMNaCl',
+    #              3: 'AuNR_5_19_E6K_10mMNaCl',
+    # }
+    name_cfn = { 1: 'AuNR_5_19_stock_100mMNaCl', # add 36.6uL 5M NaCl @11:35pm 06/30/21
+                 2: 'AuNR_5_19_T6K_100mMNaCl',
+                 3: 'AuNR_5_19_E6K_100mMNaCl',
+    }
+    yield from bps.mv(geo.det_mode,1)
+    x2_pos1 = -11.3-38.1
+    tilt1 = -0.4
+
+    x2_pos2 = -11.3-0.2
+    tilt2 = -0.4
+
+    x2_pos3 = -11.3+38.1-0.5
+    tilt3 = -0.4
+
+    yield from cfn_ref(name_cfn[1],x2_pos1,tilt1)
+    yield from cfn_gid(name_cfn[1])
+    yield from cfn_ref(name_cfn[2],x2_pos2,tilt2)
+    yield from cfn_gid(name_cfn[2])
+    yield from cfn_ref(name_cfn[3],x2_pos3,tilt3)
+    yield from cfn_gid(name_cfn[3])
 
 
 
-                
+def cfn_1():
+    '''
+    XR and GID run for one sample cell
+    '''
+    name_cfn = { 2: 'AuNR_10_50_T6K', # @11:14am 07/01/21
+    }
+    yield from bps.mv(geo.det_mode,1)
+    x2_pos2 = -11.3+1.5
+    tilt2 = -0.4
+    yield from cfn_ref(name_cfn[2],x2_pos2,tilt2)
+    yield from cfn_gid(name_cfn[2])
 
 
+def cfn_gid(name):
+
+       # sets sample height at alpha=0.08 so that it is ready for GID
+    
+    print('Start the height scan before GID')
+     # open shutter
+    # This takes the GID
+    exp_time_lst = [1, 1, 1, 2, 5] # exposure time
+    dx2_lst = [0, 0.25, 0.5, 0.75, 1] # 0.25, 0.75 and 1 are fresh spots
+    for k in range(5): 
+        _dx2 = dx2_lst[k]
+        _exp_time = exp_time_lst[k]
+        yield from bps.mvr(geo.stblx2,_dx2)
+        if _dx2 == 0:
+            yield from bps.mv(shutter,1)
+            # yield from sample_height_set_fine()
+        yield from bps.mv(geo.det_mode,2)
+        alphai = 0.1
+        yield from gid_new(md={'sample_name': name+'_GID_pos_' + str(_dx2)+'_exp_' + str(_exp_time)+'s'},
+                        exp_time = _exp_time,
+                        detector = 'pilatus300k',
+                        alphai = alphai,
+                        attenuator=0)
+
+
+def cfn_ref(name,xpos,tiltx):
+    '''Conduct reflectivity measurments'''
+    print("file name=",name)
+    yield from bps.mv(geo.stblx2,xpos)  #move the  Sample Table X2 to xpos
+    yield from bps.mv(tilt.x,tiltx)  #move the  Sample tilt 
+    yield from bps.mv(shutter,1) # open shutter
+    yield from ih_set()  #Align the spectrometer  height
+    yield from tth_set() #Align the spectrometer rotation angle
+    yield from sample_height_set_coarse() #scan the detector arm height (sh) from -1 to 1 with 41 points
+    yield from sample_height_set_fine()   #scan the detector arm height from -0.2 to 0.2 with 21 points
+    yield from bps.mv(shutter,1) # open shutter
+    # yield from astth_set()   #Align the detector arm rotation angle# comment out as it might affect OFFSET
+    yield from fast_scan(name)
 
 
 
---
+++ startup/users/97-ccny.py
@@ -0,0 +1,59 @@
+
+def ccny_ref(name,xpos):
+    '''Conduct reflectivity measurments'''
+    print("file name=",name)
+    yield from bps.mv(geo.stblx2,xpos)  #move the  Sample Table X2 to xpos
+    yield from bps.mv(shutter,1) # open shutter
+    yield from ih_set()  #Align the spectrometer stage height
+    yield from tth_set() #Align the spectrometer rotation angle
+    yield from sample_height_set_coarse() #scan the detector arm height (sh) from -1 to 1 with 41 points
+    yield from sample_height_set_fine()   #scan the detector arm height from -0.2 to 0.2 with 21 points
+    yield from astth_set()   #Align the detector arm rotation angle
+    yield from fast_scan(name)
+
+
+def ccny_xf(name,xpos):
+    print("file name=",name)
+    yield from bps.mv(abs2,2)
+    yield from bps.mv(geo.stblx2,xpos)
+    yield from bps.mv(shutter,1) # open shutter
+    yield from sample_height_set_coarse()  #scan the height from -1 to 1 with 41 points
+    yield from sample_height_set_fine() #scan the height from -0.2 to 0.2 with 21 points
+    yield from fast_scan_fluo(name)
+
+
+def ccny_ref_xf(name,xpos):       #Runs BOTH reflectivity and fluorescence
+    print("file name=",name)
+    yield from bps.mv(shutter,1)  #Open shutter
+    yield from ih_set()
+    yield from tth_set()
+    yield from bps.mv(geo.stblx2,xpos)
+    yield from sample_height_set_coarse()
+    yield from sample_height_set_fine()
+    yield from astth_set()   #Align the detector arm rotation angle
+    yield from fast_scan(name)
+    yield from fast_scan_fluo(name)
+
+
+def ccny_3():
+    yield from bps.mv(geo.det_mode,1)
+    x2_pos1 = -10-50
+    x2_pos2 = -10
+    x2_pos3 = -10+50-1
+
+    yield from ccny_ref("026_PIPES_only",x2_pos1)
+    yield from ccny_ref("027_PIPES_Ca",x2_pos2)
+    yield from ccny_ref("028_PIPES_Tb",x2_pos3)
+
+    # Alternate position if you want to run twice on the same sample
+    x2_pos1b = -10-50+2
+    x2_pos2b = -10+2
+    x2_pos3b = -10+50-2
+
+    # Alternate command for X-ray Fluorescence at the same position
+    yield from ccny_xf("028x_PIPES_tb_xf",x2_pos3b)
+    yield from ccny_xf("027x_PIPES_Ca_xf",x2_pos2b)
+    yield from ccny_xf("026x_PIPES_only_xf",x2_pos1b)
+    
+
+
