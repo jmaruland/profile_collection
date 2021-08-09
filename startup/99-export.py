15e7790adcef5ad35c47963548d99a39fb4e2649
Guillaume Freychet <gfreychet@gmail.com>
2021-08-09 15:39:23 -0400

    Fix with Tom the export file

--- startup/99-export.py
+++ startup/99-export.py
@@ -8,22 +8,35 @@ from bluesky_darkframes import DarkSubtraction
 from bluesky.callbacks.zmq import RemoteDispatcher
 import databroker.assets.handlers
 
-USERDIR = '/nsls2/xf12id1/user/2021_2/308360_vaknin/GID_data'
+USERDIR = '/nsls2/xf12id1/users/2021_2/308360_vaknin/GID_data'
 
 dispatcher = RemoteDispatcher('localhost:5578')
 
 def factory(name, start_doc):
-    if '{start[plan_name]}' == 'gid':
-        serializer = tiff_series.Serializer(file_prefix=('{start[sample_name]}_'
-                                                        ),
+    start_doc = dict(start_doc)
+    if start_doc['plan_name'] != 'gid':
+        return [], []
+
+    serializer = tiff_series.Serializer(file_prefix=('{start[sample_name]}_'),
                                             directory=USERDIR)
-        serializer('start', start_doc)
+    # serializer('start', start_doc)
 
     def subfactory(name, descriptor_doc):
+        descriptor_doc = dict(descriptor_doc)
         if descriptor_doc['name'] == 'primary':
             # Tell RunRouter to give the serializer all the data from this stream.
             serializer('descriptor', descriptor_doc)
-            return [serializer]
+            def make_doc_a_dict_and_drop_em_range(name, doc):
+                doc = dict(doc)
+                if name.startswith('event'):
+                    doc['data'] = {
+                        k: v 
+                        for k, v in doc['data'].items() 
+                        if not k.startswith('quadem')
+                    }
+                    
+                serializer(name, doc)
+            return [make_doc_a_dict_and_drop_em_range]
         else:
             # Tell RunRouter we don't care about this stream.
             return []
