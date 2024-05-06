5939c842e4bf49e78c0c5eb082bb7ea878e9451c
Benjamin Ocko <ocko@bnl.gov>
2024-05-06 16:50:04 -0400

    add new sample class of TroughChamber

--- startup/990-sample.py
+++ startup/990-sample.py
@@ -62,9 +62,10 @@ class Sample_Generic:
 
 
 class Sample(Sample_Generic):
-    '''Sample defined by the sample-related PVs'''
+    '''Sample defined by the sample-related PVs based on given slots (e.g., 'A')'''
     def __init__(self, slot):
         self.sample_epics = SampleEpicsSignal(slot)
+        self.slot = slot
         self.update()
 
     def update(self):
@@ -78,7 +79,15 @@ class Sample(Sample_Generic):
         self.gid = True if self.sample_epics.gid.get() else False
 
     def sh_offset_update(self):
+        '''Read the sh.user_offset value after the sample height alignment'''
         self.sh_offset = sh.user_offest.value if not IN_SIM_MODE else None # could not read user_offset in SIM_mode
+
+    def sh_offset_set(self):
+        '''Set the sh.user_offset value if the sample height has been aligned'''
+        if self.sh_offset is not None:
+            sh.user_offset.set(self.sh_offset)
+        else:
+            raise ValueError(f'The sh of <{self.name}> has NOT been aligned yet!') 
 
     def reset_clock(self):
         self.clock_o = time.time()
@@ -90,7 +99,75 @@ class Sample(Sample_Generic):
 
 class TroughChamber():
     '''a chamber contains multiple troughs'''
-    pass
+    
+    def __init__(self):
+        self._samples = {}
+        self.reset_clock()
+        self.addSamples()
+        self.seq_dict = {}
+        self.seq_nums = set()
+        self.addSeq()
+
+    def reset_clock(self):
+        self.clock_o = time.time()
+
+    def clock(self):
+        time_delta = time.time() - self.clock_o
+        return time_delta
+
+    def addSamples(self):
+        """Add samples from epics."""
+        for slot in ['A', 'B', 'C', 'D']:
+            self._samples[slot] = Sample(slot)
+
+    def getSamples(self):
+        '''Get sample list'''
+        samples_list = []
+        for sample_slot, sample in sorted(self._samples.items()):
+            samples_list.append(sample)
+        return samples_list
+    
+    def listSamples(self):
+        """Print a list of the current samples associated with this holder"""
+        for sample_slot, sample in sorted(self._samples.items()):
+            # print("{}: {:s} --> run: {}".format(sample_slot, sample.name, sample.run))
+            print(f"{sample_slot: <2}: {sample.name: <25} run-->", sample.run)
+
+    def addSeq(self):
+        '''Add sequence number for the run. They needs to be unique numbers.'''
+        for _sam in self.getSamples():
+            if _sam.run:
+                if _sam.seq_num in self.seq_nums:
+                    raise ValueError(f'Seq Num {_sam.seq_num} is not unique!')
+                self.seq_nums.add(_sam.seq_num)
+                self.seq_dict[_sam.seq_num] = _sam.slot
+
+    def listRuns(self):
+        '''List sample run info.'''
+        # formating
+        sample_len = []
+        for _seq in self.seq_nums:
+            sample_len.append(len(self._samples[self.seq_dict[_seq]].name))
+        sample_len_max = max(sample_len)+1
+
+        print('----------------------------------------------------------------------')
+        print('Samples to run:')
+        for _seq in self.seq_nums:
+            sample = self._samples[self.seq_dict[_seq]]
+            measure_type = [measure.upper() for measure in ['xrr', 'xrf', 'gisaxs', 'gid'] if getattr(sample,measure)]
+            print(f'Seq: {sample.seq_num}, x2 = {sample.x2:>4.1f}, {sample.name: <{sample_len_max}}: {measure_type} ')
+
+        print('----------------------------------------------------------------------')
+
+
+
+## for testing
+if False:
+    # sam = Sample('A')
+    # print(sam)
+    chamber = TroughChamber()
+    chamber.listSamples()
+    chamber.listRuns()
 
 
 
