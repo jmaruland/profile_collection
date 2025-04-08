ef58f23a2fd5835b194827540b896221cd9fc94f
Benjamin Ocko <ocko@bnl.gov>
2025-04-08 10:27:10 -0400

    update motor class

--- startup/20-motors.py
+++ startup/20-motors.py
@@ -5,8 +5,6 @@ class Table1(Device):
     z = Cpt(EpicsMotor, "TblZ}Mtr")
     x = Cpt(EpicsMotor, "TblX}Mtr")
     y = Cpt(EpicsMotor, "TblY}Mtr")
-    
-
 
 tab1 = Table1("XF:12ID1-ES{XtalDfl-Ax:", name="tab1")
 
@@ -15,9 +13,9 @@ class Tilt(Device):
     x = Cpt(EpicsMotor, "X}Mtr")
     y = Cpt(EpicsMotor, "Y}Mtr")
 
-    
 tilt = Tilt("XF:12ID1-ES{Smpl-Ax:Tilt", name="tilt")
 #Name:        XF:12ID1-ES{Smpl-Ax:TiltY}Mtr.DESC
+
 
 class DET_SAXS(Device):
     x = Cpt(EpicsMotor, "X}Mtr")
@@ -25,9 +23,11 @@ class DET_SAXS(Device):
     
 detsaxs = DET_SAXS("XF:12ID1-ES{DetSAXS-Ax:", name="det_saxs")
 
+
 class FLIGHT_PATH_SAXS(Device):
     y1 = Cpt(EpicsMotor, "Y1}Mtr")
     y2 = Cpt(EpicsMotor, "Y2}Mtr")
+
 
 fp_saxs =  FLIGHT_PATH_SAXS("XF:12ID1-ES{SAXS-Ax:", name ="flight_path_saxs")
 
@@ -37,12 +37,11 @@ o2_per = EpicsSignal("XF:12ID1:O2", name = "o2_per")
 chiller_T = EpicsSignal("XF:12ID1-ES{Chiller}BathT_RBV", name = "chiller_T")
 
 class ROT(Device):
-    rot = Cpt(EpicsMotor, "Smpl-Ax:Th}Mtr")
+    rot = Cpt(EpicsMotor, "Th}Mtr")
 
-asth =  ROT("XF:12ID1-ES{", name ="asth")
-
-
+asth =  ROT("XF:12ID1-ES{Smpl-Ax:", name ="asth")
 
 
+#    # asth = Cpt(EpicsMotor, "{Smpl-Ax:Th}Mtr, doc="Sample rotation")
 
-#asth = Cpt(EpicsMotor, "{Smpl-Ax:Th}Mtr", doc="Sample rotation", name = 'asth')
+# asth = Cpt(EpicsMotor, "XF:12ID1-ES{Smpl-Ax:Th}Mtr", doc="Sample rotation", name = 'asth')
