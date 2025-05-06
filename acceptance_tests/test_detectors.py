b5b79d3caf2304221a922ba54f45444909098637
Benjamin Ocko <ocko@bnl.gov>
2025-05-06 09:07:44 -0400

    acceptance tests

--- acceptance_tests/test_detectors.py
+++ acceptance_tests/test_detectors.py
@@ -13,8 +13,17 @@ def test_pilatus300k():
     uid, = RE(bp.count([pilatus300k]))
     _ = db[uid].table(fill=True)
 
-
-test_quadem()
-test_lambda()
-test_pilatus300k()
+def det_test(detectors=None):
+    '''
+    test all the all detectors
+    '''
+    if detectors is None:
+        detectors = [quadem, lambda_det, pilatus100kA, xs, pilatus1m]
+    yield from det_set_exposure(detectors, exposure_time=1, exposure_number = 1)
+    for det in detectors:
+        print(f'Currently running {det.name}...')
+        yield from bp.count([det])
+        yield from bps.sleep(0.1)  
+        
+det_test()
 
