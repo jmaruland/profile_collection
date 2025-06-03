ac38afe15c61058e3e08a391d0c1c3cda9294202
PLS Operator <xf12id1@xf12id1-ws1.nsls2.bnl.local>
2025-06-03 17:04:09 -0400

    Added saving to tiff files feature

--- startup/26-cameras.py
+++ startup/26-cameras.py
@@ -3,7 +3,7 @@
 from ophyd import EpicsMotor, EpicsSignal, Device, Component as C
 
 from ophyd import (ProsilicaDetector, SingleTrigger, TIFFPlugin, ImagePlugin,
-                    EpicsSignal, ROIPlugin, TransformPlugin, ProcessPlugin)
+                    EpicsSignal, ROIPlugin, TransformPlugin, ProcessPlugin, ProsilicaDetectorCam)
 
 from ophyd.areadetector.filestore_mixins import (FileStoreTIFFIterativeWrite)
 
@@ -15,8 +15,27 @@ from nslsii.ad33 import StatsPluginV33 as StatsPlugin
 class TIFFPluginWithFileStore(TIFFPlugin, FileStoreTIFFIterativeWrite):
     pass
 
+class ProsilicaDetectorCamV33(ProsilicaDetectorCam):
+    """This is used to update the standard prosilica to AD33."""
+
+    wait_for_plugins = Cpt(EpicsSignal, "WaitForPlugins", string=True, kind="config")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stage_sigs["wait_for_plugins"] = "Yes"
+
+    def ensure_nonblocking(self):
+        self.stage_sigs["wait_for_plugins"] = "Yes"
+        for c in self.parent.component_names:
+            cpt = getattr(self.parent, c)
+            if cpt is self:
+                continue
+            if hasattr(cpt, "ensure_nonblocking"):
+                cpt.ensure_nonblocking()
+
 class StandardProsilica(SingleTrigger, ProsilicaDetector):
     image = Cpt(ImagePlugin, 'image1:')
+    cam = Cpt(ProsilicaDetectorCamV33, "cam1:")
     stats1 = Cpt(StatsPlugin, 'Stats1:')
     stats2 = Cpt(StatsPlugin, 'Stats2:')
     stats3 = Cpt(StatsPlugin, 'Stats3:')
@@ -34,7 +53,6 @@ class StandardProsilica(SingleTrigger, ProsilicaDetector):
         self.hints = {'fields': [getattr(self, st).total.name]}
         self.read_attrs = [st]
 
-        
 class StandardProsilicaWithTIFF(StandardProsilica):
     tiff = Cpt(TIFFPluginWithFileStore,
                suffix='TIFF1:',
@@ -52,13 +70,15 @@ class StandardProsilicaWithTIFF(StandardProsilica):
 FS = StandardProsilicaWithTIFF('XF:12ID1-BI{Scr:1}', name='webcam-1')
 
 FS.image.kind = 'hinted'
-
 FS.tiff.kind = 'hinted' 
 
-FS.read_attrs = ['stats1', 'stats2', 'stats3', 'stats4']
+FS.read_attrs = ['stats1', 'stats2', 'stats3', 'stats4', 'tiff']
 FS.stats1.read_attrs = ['total']
 FS.stats2.read_attrs = ['total']
 FS.stats3.read_attrs = ['total']
 FS.stats4.read_attrs = ['total']
-FS.configuration_attrs = ['cam.acquire_time']
 
+FS.cam.ensure_nonblocking()
+FS.configuration_attrs = ['cam.acquire_time']
+FS.tiff.read_attrs = []
+FS.cam.ensure_nonblocking()
