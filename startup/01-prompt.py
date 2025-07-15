f0e81b54c9d96a5fba2906632844f4bae2b77fa4
Juan Marulanda Arias <jmaruland@bnl.gov>
2025-07-15 16:32:01 -0400

    Added info messages for users

--- startup/01-prompt.py
+++ startup/01-prompt.py
@@ -8,7 +8,7 @@ class ProposalIDPrompt(Prompts):
             (Token.CursorLine, f"{datetime.datetime.isoformat(datetime.datetime.now(), timespec='seconds')} - "),
             (
                 Token.Prompt,
-                f"{RE.md.get('data_session', 'N/A')} [",
+                f"{RE.md.get('data_session', 'N/A')} - {RE.md.get('project_name', 'N/A')} [",
             ),
             (Token.PromptNum, str(self.shell.execution_count)),
             (Token.Prompt, '] ' + u"\u25B6" + ' '),
